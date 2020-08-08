ec8f86b88aacd954435228a20c02811b26c1430f
Hasbi Assidiqqi <56348231+Hasbi-Assidiqqi@users.noreply.github.com>
2020-08-08 14:43:52 +0700

    Add files via Uploud
    Added the php file for the Android LoginRegister Program API (Android / Java / LoginRegister). place this file in the xampp \ htdocs folder.

---
+++ API LoginRegister - Android/android_register_login/register.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $nim = $_POST['nim'];
+    $password = $_POST['password'];
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    require_once 'connect.php';
+
+    $sql = "INSERT INTO users_table (username, email, nim, password) VALUES ('$username', '$email', '$nim', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+        $result["success"] = "1";
+        $result["message"] = "success";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    } else {
+        $result["success"] = "0";
+        $result["message"] = "error";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+}
